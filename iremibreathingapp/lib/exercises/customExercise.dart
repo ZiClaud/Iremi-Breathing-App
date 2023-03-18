43793845c03d18f49cf25a316ff227b1998af839
Claudio Di Maio <claudiodimaio01@gmail.com>
2023-03-18 16:39:02 +0100

    Added Icon to Android and changed android name application

--- iremibreathingapp/lib/exercises/customExercise.dart
+++ iremibreathingapp/lib/exercises/customExercise.dart
@@ -19,8 +19,8 @@ class CustomExercise extends Exercise {
   @override
   int times;
 
-  CustomExercise(
-      this.steps, inhaleTime, holdMiddleTime, exhaleTime, holdEndTime, this.times) {
+  CustomExercise(this.steps, inhaleTime, holdMiddleTime, exhaleTime,
+      holdEndTime, this.times) {
     this.inhaleTime = Duration(seconds: inhaleTime);
     this.holdMiddleTime = Duration(seconds: holdMiddleTime);
     this.exhaleTime = Duration(seconds: exhaleTime);
