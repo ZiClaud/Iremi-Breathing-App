a87b2be93a97b02592143165d6d647c0869e8105
Claudio Di Maio <claudiodimaio01@gmail.com>
2023-03-19 13:30:17 +0100

    Cleaned code

--- iremibreathingapp/test/widget_test.dart
+++ iremibreathingapp/test/widget_test.dart
@@ -5,10 +5,9 @@
 // gestures. You can also use WidgetTester to find child widgets in the widget
 // tree, read text, and verify that the values of widget properties are correct.
 
+/*
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
-
-import 'package:iremibreathingapp/pages/demoPage.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
@@ -28,3 +27,4 @@ void main() {
     expect(find.text('1'), findsOneWidget);
   });
 }
+*/
