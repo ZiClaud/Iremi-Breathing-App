a81094ff1c5ab0aaa9db7f841999a9b1437bf0e7
Claudio Di Maio <claudiodimaio01@gmail.com>
2023-03-19 18:22:55 +0100

    Improved code When exercise is done, it goes back to the previous page

--- iremibreathingapp/lib/pages/exercisePage.dart
+++ iremibreathingapp/lib/pages/exercisePage.dart
@@ -3,6 +3,7 @@ import 'package:iremibreathingapp/basics/exercises/exercise.dart';
 
 import '../utils/defaultWidget.dart';
 import '../utils/theme.dart';
+import 'exerciseDetailsPage.dart';
 
 class ExercisePage extends StatefulWidget {
   Exercise exercise;
@@ -22,12 +23,104 @@ class _ExercisePageState extends State<ExercisePage> {
           title: Text(exercise.name),
           backgroundColor: myBluLight,
         ),
-        body: FourStageAnimation(
-          durationA: exercise.inhaleDuration,
-          durationB: exercise.holdMiddleDuration,
-          durationC: exercise.exhaleDuration,
-          durationD: exercise.holdEndDuration,
-          times: exercise.times,
-        ));
+        body: _FourStageAnimation(exercise: exercise));
+  }
+}
+
+class _FourStageAnimation extends StatefulWidget {
+  final Exercise exercise;
+
+  _FourStageAnimation({
+    required this.exercise,
+  });
+
+  @override
+  _FourStageAnimationState createState() => _FourStageAnimationState();
+}
+
+class _FourStageAnimationState extends State<_FourStageAnimation>
+    with SingleTickerProviderStateMixin {
+  late AnimationController _controller;
+  late Animation<Size> _animation;
+  int _counter = 0;
+
+  @override
+  void initState() {
+    super.initState();
+    _controller = AnimationController(
+      vsync: this,
+      duration: widget.exercise.inhaleDuration +
+          widget.exercise.holdMiddleDuration +
+          widget.exercise.exhaleDuration +
+          widget.exercise.holdEndDuration,
+    );
+
+    _animation = TweenSequence([
+      TweenSequenceItem(
+        tween: Tween<Size>(begin: Size.zero, end: Size.square(200)),
+        weight: widget.exercise.inhaleDuration.inMilliseconds.toDouble() /
+            _controller.duration!.inMilliseconds.toDouble(),
+      ),
+      TweenSequenceItem(
+        tween: Tween<Size>(begin: Size.square(200), end: Size.square(200)),
+        weight: widget.exercise.holdMiddleDuration.inMilliseconds.toDouble() /
+            _controller.duration!.inMilliseconds.toDouble(),
+      ),
+      TweenSequenceItem(
+        tween: Tween<Size>(begin: Size.square(200), end: Size.zero),
+        weight: widget.exercise.exhaleDuration.inMilliseconds.toDouble() /
+            _controller.duration!.inMilliseconds.toDouble(),
+      ),
+      TweenSequenceItem(
+        tween: Tween<Size>(begin: Size.zero, end: Size.zero),
+        weight: widget.exercise.holdEndDuration.inMilliseconds.toDouble() /
+            _controller.duration!.inMilliseconds.toDouble(),
+      ),
+    ]).animate(_controller);
+
+    // Start the loop
+    _loopAnimation();
+  }
+
+  void _loopAnimation() async {
+    while (_counter < widget.exercise.times) {
+      // Run the animation once
+      await _controller.forward().orCancel;
+
+      // Increment the counter
+      _counter++;
+
+      // Reset the animation if it's not the last iteration
+      if (_counter < widget.exercise.times) {
+        _controller.reset();
+      } else {
+        Navigator.pop(context);
+      }
+    }
+  }
+
+  @override
+  void dispose() {
+    _controller.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: AnimatedBuilder(
+        animation: _animation,
+        builder: (BuildContext context, Widget? child) {
+          return Container(
+            width: _animation.value.width,
+            height: _animation.value.height,
+            decoration: BoxDecoration(
+              color: myBluLight,
+              shape: BoxShape.circle,
+            ),
+          );
+        },
+      ),
+    );
   }
 }
