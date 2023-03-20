7bb637be7ac0952b0d6ec736bff62aceafaafa06
Claudio Di Maio <claudiodimaio@gmail.com>
2023-03-20 14:45:24 +0100

    Added Register Page Removed Login page (it's useless)
    Starting on working on DB

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:iremibreathingapp/pages/mainPage.dart';
+import 'package:iremibreathingapp/pages/registerPage.dart';
 import 'package:iremibreathingapp/utils/theme.dart';
 
 void main() {
@@ -16,7 +17,7 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: myBluLightMaterial,
       ),
-      home: const MainPage(),
+      home: const RegisterPage(),
     );
   }
 }
