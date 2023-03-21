905568f769a5f8572bda5b36383b31871b5da63a
Claudio Di Maio <56027915+ZiClaud@users.noreply.github.com>
2023-03-21 21:24:25 +0100

    When you start the app, if there's no user, it will start on registerPageDB, if there is, it will start on homePage

--- lib/database/test/dbTestAddPage.dart
+++ lib/database/test/dbTestAddPage.dart
@@ -4,7 +4,7 @@ import 'package:iremibreathingapp/basics/user.dart';
 import 'package:iremibreathingapp/database/database.dart';
 import 'package:iremibreathingapp/utils/theme.dart';
 
-import '../../utils/defaultWidget.dart';
+import '../forms/userFormWidget.dart';
 
 class TestDBAddUserPage extends StatefulWidget {
   final MyUser? user;
@@ -205,90 +205,5 @@ class MyApp extends StatelessWidget {
           ),
         ),
         home: TestDBAddUserPage(),
-      );
-}
-
-class UserFormWidget extends StatelessWidget {
-  final String? username;
-  final String? name;
-  final String? surname;
-  final String? sex;
-  final String? goal;
-  final ValueChanged<String> onChangedUsername;
-  final ValueChanged<String> onChangedName;
-  final ValueChanged<String> onChangedSurname;
-  final ValueChanged<String> onChangedSex;
-  final ValueChanged<String> onChangedGoal;
-
-  const UserFormWidget({
-    Key? key,
-    this.username = '',
-    this.name = '',
-    this.surname = '',
-    this.sex = '',
-    this.goal = '',
-    required this.onChangedUsername,
-    required this.onChangedName,
-    required this.onChangedSurname,
-    required this.onChangedSex,
-    required this.onChangedGoal,
-  }) : super(key: key);
-
-  @override
-  Widget build(BuildContext context) => SingleChildScrollView(
-        child: Padding(
-          padding: EdgeInsets.all(16),
-          child: Column(
-            mainAxisSize: MainAxisSize.min,
-            children: [
-              Column(
-                children: [
-                  TextFormField(
-                    decoration:
-                        defaultInputDecoration("Username", Icons.person),
-                    keyboardType: TextInputType.multiline,
-                    maxLines: null,
-                    onChanged: onChangedUsername,
-                    validator: (username) =>
-                        username != null && username.isEmpty
-                            ? 'The username cannot be empty'
-                            : null,
-                  ),
-                  TextFormField(
-                    decoration: defaultInputDecoration(
-                        "Name", Icons.nest_cam_wired_stand),
-                    keyboardType: TextInputType.multiline,
-                    maxLines: null,
-                    onChanged: onChangedName,
-                    validator: (name) => name != null && name.isEmpty
-                        ? 'The name cannot be empty'
-                        : null,
-                  ),
-                  TextFormField(
-                    decoration: defaultInputDecoration(
-                        "Surname (optional)", Icons.surfing),
-                    keyboardType: TextInputType.multiline,
-                    maxLines: null,
-                    onChanged: onChangedSurname,
-                  ),
-                  TextFormField(
-                    decoration:
-                        defaultInputDecoration("Sex (optional)", Icons.male),
-                    keyboardType: TextInputType.multiline,
-                    maxLines: null,
-                    onChanged: onChangedSex,
-                  ),
-                  TextFormField(
-                    decoration: defaultInputDecoration(
-                        "Goal (optional)", Icons.circle_outlined),
-                    keyboardType: TextInputType.multiline,
-                    maxLines: null,
-                    onChanged: onChangedGoal,
-                  ),
-                ],
-              ),
-            ],
-          ),
-        ),
       );
 }
