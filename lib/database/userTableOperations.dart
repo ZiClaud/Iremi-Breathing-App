321e9b431ca72357f5c28b1ccf206b0daa14e743
Claudio Di Maio <56027915+ZiClaud@users.noreply.github.com>
2023-03-22 15:33:07 +0100

    I now use a defaultLoadingScreen instead of just MyBluLight while charging db

--- lib/database/userTableOperations.dart
+++ lib/database/userTableOperations.dart
@@ -1,3 +1,23 @@
-Type getObjectType(Type object) {
-  return object;
+/*
+import '../basics/user.dart';
+
+Future<MyUser> createUser(MyUser user) async {
+  return _create(user);
 }
+
+Future<MyUser?> getFirstUser() async {
+  return _getFirstUser();
+}
+
+Future<List<MyUser>> readAllUsers() async {
+  return _readAllUsers();
+}
+
+Future updateUser(MyUser user) async {
+  _update(user);
+}
+
+Future deleteUser(MyUser user) async {
+  _delete(user.id!);
+}
+ */
