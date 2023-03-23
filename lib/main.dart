004f6e6fe1cf276074b94655e60c3f88a16752aa
Claudio Di Maio <56027915+ZiClaud@users.noreply.github.com>
2023-03-23 08:50:58 +0100

    Working on dark theme

--- lib/main.dart
+++ lib/main.dart
@@ -10,17 +10,69 @@ void main() {
   runApp(const MyApp());
 }
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
   const MyApp({Key? key}) : super(key: key);
+
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  final IremiTheme _iremiTheme = IremiTheme();
+
+  @override
+  void initState() {
+    super.initState();
+    _iremiTheme.addListener(_onThemeChanged);
+  }
+
+  void _onThemeChanged() {
+    setState(() {});
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      theme: IremiTheme.lightTheme,
+      darkTheme: IremiTheme.darkTheme,
+      themeMode: _iremiTheme.iremiTheme,
+      home: FutureBuilder<Widget>(
+        future: getHomePage(),
+        builder: (BuildContext context, AsyncSnapshot<Widget> snapshot) {
+          if (snapshot.hasData) {
+            return snapshot.data!;
+          } else {
+            return defaultLoadingScreen();
+          }
+        },
+      ),
+    );
+  }
+}
+
+
+class _MyAppState2 extends State<MyApp> {
+  final IremiTheme _iremiTheme = IremiTheme();
+
+  @override
+  void initState() {
+    super.initState();
+    _iremiTheme.addListener(_onThemeChanged);
+  }
+
+  void _onThemeChanged() {
+    setState(() {});
+  }
 
   @override
   Widget build(BuildContext context) {
     // TODO: READ SETTINGS TABLE - IF IT DOESN'T EXIST CREATE ONE
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      theme: ThemeData(
-        primarySwatch: myBluLightMaterial,
-      ),
+      theme: IremiTheme.lightTheme,
+      darkTheme: IremiTheme.darkTheme,
+      themeMode: _iremiTheme.iremiTheme,
       home: FutureBuilder<Widget>(
         future: getHomePage(),
         builder: (BuildContext context, AsyncSnapshot<Widget> snapshot) {
--- lib/utils/theme.dart
+++ lib/utils/theme.dart
@@ -5,6 +5,8 @@ const Color myBlu = Color(0xFF01B3CA);
 const Color myBluDark = Color(0xFF007C9A);
 
 const Color myWhite = Color(0xFFFFFFFF);
+const Color myLightBackgroundColor = Color(0xFFFFFFFF);
+const Color myDarkBackgroundColor = Color(0xFF000000);
 const Color myBlack = Color(0xFF000000);
 
 const Map<int, Color> myColorMapLight = {
@@ -37,3 +39,132 @@ const MaterialColor myBluLightMaterial =
     MaterialColor(0xFF3B44AC, myColorMapLight);
 const MaterialColor myBluDarkMaterial =
     MaterialColor(0xFF3B44AC, myColorMapDark);
+
+/// ---------------------------
+
+class IremiTheme with ChangeNotifier {
+  static bool _isDarkTheme = false;
+
+  ThemeMode get iremiTheme => _isDarkTheme ? ThemeMode.dark : ThemeMode.light;
+
+  void toggleTheme() {
+    _isDarkTheme = !_isDarkTheme;
+    notifyListeners();
+  }
+
+  setMode(bool darkmode) {
+    _isDarkTheme = darkmode;
+    notifyListeners();
+  }
+
+  static ThemeData get lightTheme {
+    return ThemeData(
+      primarySwatch: myBluLightMaterial,
+      scaffoldBackgroundColor: myLightBackgroundColor,
+      appBarTheme: const AppBarTheme(
+        backgroundColor: myBluLight,
+//          foregroundColor: Colors.white,
+      ),
+      /*
+        bottomNavigationBarTheme: const BottomNavigationBarThemeData(
+          backgroundColor: JEMORE_BLUE,
+        ),
+        primaryColor: JEMORE_BLUE,
+        backgroundColor: Colors.white,
+        scaffoldBackgroundColor: Colors.white,
+        iconTheme: const IconThemeData(
+          //TODO: FIX
+          color: JEMORE_BLUE,
+        ),
+        timePickerTheme: const TimePickerThemeData(
+          //TODO: FIX
+          //backgroundColor: JEMORE_BLUE,
+          backgroundColor: Colors.white,
+          // hourMinuteColor: JEMORE_BLUE,
+          hourMinuteTextColor: JEMORE_BLUE,
+        ),
+        */
+//        unselectedWidgetColor: JEMORE_YELLOW,
+//        fontFamily: 'Gotham',
+      /*
+        textTheme: const TextTheme(
+          headline1: TextStyle(
+              color: Colors.black, fontSize: 20, fontFamily: 'Gotham'),
+          headline2: TextStyle(
+              color: Colors.black, fontSize: 20, fontFamily: 'Gotham'),
+          headline3: TextStyle(
+              color: Colors.black, fontSize: 20, fontFamily: 'Gotham'),
+          headline4: TextStyle(
+              color: Colors.black, fontSize: 20, fontFamily: 'Gotham'),
+          headline5: TextStyle(
+              color: Colors.black, fontSize: 20, fontFamily: 'Gotham'),
+          headline6: TextStyle(
+              color: Colors.black, fontSize: 20, fontFamily: 'Gotham'),
+          bodyText1: TextStyle(
+              color: Colors.black, fontSize: 20, fontFamily: 'Gotham'),
+          bodyText2: TextStyle(
+              color: Colors.black, fontSize: 20, fontFamily: 'Gotham'),
+          subtitle1: TextStyle(
+              color: Colors.black, fontSize: 20, fontFamily: 'Gotham'),
+          subtitle2: TextStyle(
+              color: Colors.black, fontSize: 20, fontFamily: 'Gotham'),
+          caption: TextStyle(
+              color: Colors.black, fontSize: 20, fontFamily: 'Gotham'),
+          button: TextStyle(
+              color: Colors.black, fontSize: 20, fontFamily: 'Gotham'),
+        )
+    */
+    );
+  }
+
+  static ThemeData get darkTheme {
+    return ThemeData(
+      primarySwatch: myBluDarkMaterial,
+      backgroundColor: myDarkBackgroundColor,
+      scaffoldBackgroundColor: myDarkBackgroundColor,
+      appBarTheme: const AppBarTheme(
+        backgroundColor: myBluDark,
+//          foregroundColor: Colors.white,
+      ),
+      /*
+        textTheme: const TextTheme(
+          headline1: TextStyle(
+              color: Colors.white, fontSize: 20, fontFamily: 'Gotham'),
+          headline2: TextStyle(
+              color: Colors.white, fontSize: 20, fontFamily: 'Gotham'),
+          headline3: TextStyle(
+              color: Colors.white, fontSize: 20, fontFamily: 'Gotham'),
+          headline4: TextStyle(
+              color: Colors.white, fontSize: 20, fontFamily: 'Gotham'),
+          headline5: TextStyle(
+              color: Colors.white, fontSize: 20, fontFamily: 'Gotham'),
+          headline6: TextStyle(
+              color: Colors.white, fontSize: 20, fontFamily: 'Gotham'),
+          bodyText1: TextStyle(
+              color: Colors.white, fontSize: 20, fontFamily: 'Gotham'),
+          bodyText2: TextStyle(
+              color: Colors.white, fontSize: 20, fontFamily: 'Gotham'),
+          subtitle1: TextStyle(
+              color: Colors.white, fontSize: 20, fontFamily: 'Gotham'),
+          subtitle2: TextStyle(
+              color: Colors.white, fontSize: 20, fontFamily: 'Gotham'),
+          caption: TextStyle(
+              color: Colors.white, fontSize: 20, fontFamily: 'Gotham'),
+          button: TextStyle(
+              color: Colors.white, fontSize: 20, fontFamily: 'Gotham'),
+        )
+    */
+    );
+  }
+
+  static bool isDarkTheme() {
+    return _isDarkTheme;
+  }
+
+/*
+  static OutlineInputBorder detailsOutlineInputBorder() {
+    return const OutlineInputBorder(
+        borderSide: BorderSide(width: 2, color: blueJEMORE));
+  }
+  */
+}
