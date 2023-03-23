8e70aa1f6a4af594000c988a2920089983144df2
Claudio Di Maio <56027915+ZiClaud@users.noreply.github.com>
2023-03-23 16:29:20 +0100

    exerciseAddPage.dart now shows complex too

--- lib/pages/exerciseAddPage.dart
+++ lib/pages/exerciseAddPage.dart
@@ -10,23 +10,46 @@ class ExerciseAddPage extends StatefulWidget {
 }
 
 class _ExerciseAddPageState extends State<ExerciseAddPage> {
+  bool _showComplex = false;
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: Text("Add custom exercise")),
-      // TODO: Add advanced - Shows milliseconds
+      appBar: AppBar(
+        title: Text("Add custom exercise"),
+        actions: [
+          IconButton(
+            icon: const Icon(Icons.lightbulb_circle),
+            onPressed: () {
+              setState(() {
+                _showComplex = !_showComplex;
+              });
+            },
+          ),
+        ],
+      ),
       body: ListView(
         children: [
           defaultEditTextFormField("Name", Icons.keyboard_double_arrow_up),
           defaultEditTextFormField("Description", Icons.description),
           defaultEditTextFormField("Notes", Icons.note),
-          defaultEditTextFormField("Steps", Icons.select_all),
+          defaultEditTextFormField("Steps", Icons.square_foot), // TODO
+          defaultEditTextFormField("Times", Icons.incomplete_circle),
           defaultEditTextFormField("InhaleDuration", Icons.circle),
+          if (_showComplex)
+            defaultEditTextFormField("InhaleDuration (ms)", Icons.circle), // TODO: Make icon smaller
           defaultEditTextFormField("HoldMiddleDuration", Icons.change_circle),
+          if (_showComplex)
+            defaultEditTextFormField(
+                "HoldMiddleDuration (ms)", Icons.change_circle), // TODO: Make icon smaller
           defaultEditTextFormField("ExhaleDuration", Icons.circle_outlined),
+          if (_showComplex)
+            defaultEditTextFormField(
+                "ExhaleDuration (ms)", Icons.circle_outlined), // TODO: Make icon smaller
           defaultEditTextFormField(
               "HoldEndDuration", Icons.change_circle_outlined),
-          defaultEditTextFormField("Times", Icons.repeat),
+          if (_showComplex)
+            defaultEditTextFormField(
+                "HoldEndDuration (ms)", Icons.change_circle_outlined), // TODO: Make icon smaller
         ],
       ),
       floatingActionButton: FloatingActionButton(
