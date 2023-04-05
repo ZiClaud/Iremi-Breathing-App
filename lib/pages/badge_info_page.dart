283c2fc505fd047a16d0dcc79a622197b888ad1a
Claudio Di Maio <56027915+ZiClaud@users.noreply.github.com>
2023-04-05 13:56:37 +0200

    bug fix

--- lib/pages/badge_info_page.dart
+++ lib/pages/badge_info_page.dart
@@ -3,15 +3,20 @@ import 'package:iremibreathingapp/utils/theme.dart';
 
 import '../basics/badge.dart';
 
-// TODO: Fix the size, it occupies the whole screen
-
 showBadgeDialog(context, MyBadge badge) {
   return showDialog(
     context: context,
     builder: (BuildContext context) {
       return AlertDialog(
-        title: Text(badge.getBadge().badgeName),
-        content: Center(child: _showBadgeWidget(badge)),
+        title: Center(
+          child: Text(
+            badge.getBadge().badgeName,
+            style: TextStyle(fontWeight: FontWeight.bold),
+          ),
+        ),
+        contentPadding:
+            const EdgeInsets.symmetric(horizontal: 16.0, vertical: 8.0),
+        content: _showBadgeWidget(badge),
       );
     },
   );
@@ -21,24 +26,21 @@ Widget _showBadgeWidget(MyBadge badge) {
   return Column(
     mainAxisSize: MainAxisSize.min,
     children: [
-      Center(
-        child: Icon(
-          badge.getBadge().icon,
-          size: 100,
-          color: (badge.date != "") ? myBluLightDark() : myLockedBadgeColor,
-        ),
-      ),
-      Padding(
-        padding: const EdgeInsets.all(8.0),
-        child: Text(badge.getBadge().description),
+      Icon(
+        badge.getBadge().icon,
+        size: 100,
+        color: (badge.date != "") ? myBluLightDark() : myLockedBadgeColor,
       ),
       Padding(
         padding: const EdgeInsets.all(8.0),
         child: Text(
-          (badge.date != "")
-              ? "Unlocked on " + badge.date
-              : "Locked.",
+          badge.getBadge().description,
         ),
+      ),
+      Padding(
+        padding: const EdgeInsets.only(top: 24.0),
+        child:
+            Text((badge.date != "") ? "Unlocked on ${badge.date}" : "Locked."),
       ),
     ],
   );
