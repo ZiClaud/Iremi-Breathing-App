020e1ae35f1df31487ac356597f15e36112ba555
Claudio Di Maio <56027915+ZiClaud@users.noreply.github.com>
2026-02-21 12:47:06 +0100

    Changed com.ziclaud to eu.ziclaud

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -6,7 +6,7 @@ plugins {
 }
 
 android {
-    namespace = "com.ziclaud.iremibreathingapp"
+    namespace = "eu.ziclaud.iremibreathingapp"
     compileSdk = flutter.compileSdkVersion
     ndkVersion = "27.0.12077973"
 
@@ -21,7 +21,7 @@ android {
 
     defaultConfig {
         // TODO: Specify your own unique Application ID (https://developer.android.com/studio/build/application-id.html).
-        applicationId = "com.ziclaud.iremibreathingapp"
+        applicationId = "eu.ziclaud.iremibreathingapp"
         // You can update the following values to match your application needs.
         // For more information, see: https://flutter.dev/to/review-gradle-config.
         minSdk = flutter.minSdkVersion
